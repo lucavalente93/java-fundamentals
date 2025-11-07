70bcd25f28f35e3c542b2a377936b5bc155a71e1
Luca Valente <contact.lucamarins@gmail.com>
2025-11-07 18:03:56 -0300

    Refactor: removed redundancies and magic numbers

--- intellij/mortgage-calculator/src/Main.java
+++ intellij/mortgage-calculator/src/Main.java
@@ -1,32 +1,39 @@
 import java.util.Scanner;
+import java.text.NumberFormat;
+
 // Note: This program requires Java 25 or newer.
 // For Java 24 or earlier, use the standard entry point:
 // public static void main(String[] args) {}
-void main() {
-//    NumberFormat currency = NumberFormat.getCurrencyInstance();
 
+void main() {
     // Principal
-    Scanner sc1 = new Scanner(System.in);
+    final byte MONTHS_IN_YEAR = 12;
+    final byte PERCENT = 100;
+    Scanner scanner = new Scanner(System.in);
     System.out.print("Principal:");
-    double principal = sc1.nextDouble();
+    double principal = scanner.nextDouble();
     System.out.println("$" + principal);
 
     // Annual interest rate
-    Scanner sc2 = new Scanner(System.in);
     System.out.println("Insert your annual interest rate (in %):");
-    double annualRate = sc2.nextDouble() / 100; // decimal conversion
+    double annualRate = scanner.nextDouble() / PERCENT; // decimal conversion
 
     // Monthly interest rate
-    System.out.print("Monthly interest rate:");
-    double monthlyRate = (annualRate / 12);
+    System.out.println("Monthly interest rate:");
+    double monthlyInterest = (annualRate / MONTHS_IN_YEAR);
 
     // Annual and Monthly payments
-    Scanner sc3 = new Scanner(System.in);
     System.out.println("Period (in years): ");
-    int years = sc3.nextInt();
-    int totalPayments = years * 12; // months conversion
+    int years = scanner.nextInt();
+    int totalPayments = years * MONTHS_IN_YEAR;
 
     double mortgage = principal
-            * (monthlyRate * Math.pow(1 + monthlyRate, totalPayments)) / (Math.pow(1 + monthlyRate, totalPayments) - 1);
-    System.out.println("Monthly payment: $" + Math.round(mortgage));
+            * (monthlyInterest * Math.pow(1 + monthlyInterest, totalPayments))
+            / (Math.pow(1 + monthlyInterest, totalPayments) - 1);
+    System.out.println("Mortgage: $" + Math.round(mortgage));
+
+    // Currency formatting
+    //    NumberFormat currency = NumberFormat.getCurrencyInstance();
+    //    String mortgageFormatted = NumberFormat.getCurrencyInstance();
+    //    System.out.println("Mortgage: " + mortgageFormatted);
 }
